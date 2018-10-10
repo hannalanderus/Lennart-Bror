fd204a7f7cce92d1d0f9dcc8130483b805e5f697
Hanna Landerus <you@example.com>
2018-10-10 10:09:38 +0200

    add owl carousel

--- js/script.js
+++ js/script.js
@@ -34,30 +34,6 @@ function scrollFunction() {
         }
  /********* change image in Menu ********/
 
-let imgs = ["images/LB_Snacks_fix.jpg",
-            "images/LB_mat_fix.jpg",
-            "images/LB_dryck_fix.jpg"];
-
-    function changeMenuFunction() {
-        document.getElementById("menuCardimg").src = imgs[1];
-        imgs.push(imgs.shift())
-    }   
-
-/*********    Change Menu Image      ************/ 
-   /* function changeMenuFunction(dir) {
-         let img = document.getElementById("menuCardimg");
-         let img = document.getElementById("changeImageArrow");
-        img.src = imgs[imgs.indexOf(img.src) + (dir || 1)] || imgs[dir ? imgs.length - 1 : 0];
-    }
- 
-    document.onkeydown = function(e) {
-        e = e || window.event;
-        if (e.keyCode == '37') {
-            changeMenuFunction(-1) //left <- show Prev image
-        } else if (e.keyCode == '38') {
-            changeMenuFunction() // right -> show next image
-        }
-    }*/
 /****************************************************/
 
 let vh = window.innerHeight * 0.01;
